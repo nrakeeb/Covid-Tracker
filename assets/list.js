2b73aad3d3b094c7187389e14d9477513b68264b
Mohammad Zafar <sz7861@outlook.com>
2021-07-22 21:00:22 +0100

    add g7 country list + iso codes

---
+++ assets/list.js
@@ -0,0 +1,10 @@
+// declare variables as a list of countries with their equivalent ISO codes
+var country_list = [
+    { name: 'Canada', code: 'CAN' },
+    { name: 'France', code: 'FR' },
+    { name: 'Germany', code: 'DE' },
+    { name: 'Italy', code: 'IT' },
+    { name: 'Japan', code: 'JP' },
+    { name: 'UK', code: 'GB' },
+    { name: 'US', code: 'US' },
+];
