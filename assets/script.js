416bc50c3779208dfa80fb789fda42e12093476b
Nouha Rakeeb <nouha_rakeeb@hotmail.co.uk>
2021-07-23 22:27:22 +0100

    Feat: declaring variables
    Authors: NR

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+// declaring variables for all measured statistics
+var countryNameEl = $(".country .name")[0];
+var totalCasesEl = $(".total-cases .value")[0];
+var newCasesEl = $(".total-cases .new-value")[0];
+var recoveredEl = $(".recovered .value")[0];
+var newRecoveredEl = $(".recovered .new-value")[0];
+var deathsEl = $(".deaths .value")[0];
+var newDeathsEl = $(".deaths .new-value")[0];
+​
+var ctx = document.getElementById("axesLineChart").getContext("2d");
+
