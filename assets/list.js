79213e56b25f3fd667c403afd9a7c9a09c876af7
Mustafa Ali <mustafamire@gmail.com>
2021-07-24 10:12:06 +0100

    changes made to variable names lines

--- assets/list.js
+++ assets/list.js
@@ -10,25 +10,26 @@ var country_list = [
 ];
 console.log(country_list)
 
-// add search country element 
-var search_country_element = document.querySelector(".search");
+
+var searchCountry = document.querySelector(".search");
 // add country list element
-var country_list_element = document.querySelector(".g7-list");
+var countryList = document.querySelector(".g7-list");
 // add button to change country
-var chang_country_btn = document.querySelector(".change");
+var changCountry = document.querySelector(".change");
 // add button to close list
-var close_list_btn = document.querySelector(".close");
+var closeList = document.querySelector(".close");
 // add input to acknowledge searches
 var input = document.getElementById('search-input');
 
+
 // add function to display country list when searching
-function createCountryList(){
+function createCountryList() {
     // set list to equal total number of countries added
     var num_countries = country_list.length;
     // unordered country list id
     for (var i = 0; i < country_list.length; i++) {
-    console.log("test")
+        console.log("test")
     }
 }
-    
+
 
